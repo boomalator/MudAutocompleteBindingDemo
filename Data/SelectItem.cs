1c28c1786dffd9db7c6f63be52178bc365a35409
BP (Home) <bph@byronetta.com>
2021-05-14 21:02:51 -0600

    Add project files.

---
+++ Data/SelectItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DefaultBlazor
+{
+    public class SelectItemInt
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Text}".Trim();
+        }
+    }
+
+}
